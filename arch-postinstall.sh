0cbfeaa558dfcdb611470109b10e1385f1af60f7
Yuri Fontella <yurifc4@gmail.com>
2020-04-23 23:10:43 -0300

    update

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -3,7 +3,7 @@
 ## REFLECTOR
 
 pacman -Sy
-pacman -S reflector -y
+pacman -S reflector --noconfirm
 
 reflector --score 5 --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
@@ -24,13 +24,17 @@ LANG=pt_BR.UTF-8
 LANGUAGE=pt_BR
 EOF
 
+touch /etc/vconsole.conf
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+
 ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
 hwclock --utc --systohc
 timedatectl set-ntp true
 
 # DESKTOP
 
-pacman -S plasma-desktop plasma-meta sddm sddm-kcm dolphin konsole ark gwenview xdg-user-dirs
+pacman -S plasma-desktop plasma-meta sddm sddm-kcm dolphin konsole ark gwenview firefox firefox-i18n-pt-br xdg-user-dirs --noconfirm
 
 # SERVIÇOS
 
@@ -39,7 +43,7 @@ systemctl enable NetworkManager
 
 # GRUB
 
-pacman -S grub
+pacman -S grub --noconfirm
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -48,4 +52,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 useradd -m -g users -G wheel,storage,power,video -s /bin/bash yuri
 
-echo -e "yuri\nyuri" | passwd yuri 
+echo -e "yuri\nyuri" | passwd
+echo -e "yuri\nyuri" | passwd yuri
+
+sed -i -- 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
