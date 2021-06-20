fe1c3b6221d9eefaa3cd59d977cdf9d48f14c2b7
Yuri Fontella <yurifc4@gmail.com>
2021-06-20 04:38:56 -0300

    Update systemd-boot.sh

--- systemd-boot.sh
+++ systemd-boot.sh
@@ -39,7 +39,15 @@ locale-gen
 
 cat <<EOF > /etc/locale.conf
 LANG=pt_BR.UTF-8
-LANGUAGE=pt_BR
+LC_ADDRESS=pt_BR.UTF-8
+LC_IDENTIFICATION=pt_BR.UTF-8
+LC_MEASUREMENT=pt_BR.UTF-8
+LC_MONETARY=pt_BR.UTF-8
+LC_NAME=pt_BR.UTF-8
+LC_NUMERIC=pt_BR.UTF-8
+LC_PAPER=pt_BR.UTF-8
+LC_TELEPHONE=pt_BR.UTF-8
+LC_TIME=pt_BR.UTF-8
 EOF
 
 echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
